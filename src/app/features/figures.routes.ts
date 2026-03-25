de6fda02ef6b4190ac865b2d0b8bddc42c84161d
zeek-hub <remigiusz.bianga2@gmail.com>
2026-03-25 20:51:14 +0100

    Quick fixes.

--- src/app/features/figures.routes.ts
+++ src/app/features/figures.routes.ts
@@ -1,9 +1,16 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {SquarePage} from './figures/pages/square-page/square-page';
 import {TrianglePage} from './figures/pages/triangle-page/triangle-page';
+import {NgModule} from "@angular/core";
 
 export const FIGURES_ROUTES: Routes = [
   { path: 'square', component: SquarePage },
   { path: 'triangle', component: TrianglePage },
   { path: '', redirectTo: '/square', pathMatch: 'full' },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(FIGURES_ROUTES, {useHash: true})],
+  exports: [RouterModule]
+})
+export class FigureRoutingModule {}
